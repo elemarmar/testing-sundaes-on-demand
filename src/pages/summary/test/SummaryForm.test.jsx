6d054ee9d11ed26e1903b8e9b8deccc73b8d25d8
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2021-08-29 23:45:31 +0200

    Add test and code popover summary form

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
@@ -29,4 +33,27 @@ test('Checkbox disables button on first click and enables on second click', () =
   // clicking checkbox a second time disables the button
   userEvent.click(checkboxElement);
   expect(buttonElement).toBeDisabled();
+});
+test('popover responds to hover', async () => {
+  // popover starts out hidden
+  render(<SummaryForm />);
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const checkboxElement = screen.getByText(/terms and conditions/i);
+  userEvent.hover(checkboxElement);
+  const popoverElement = screen.getByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(popoverElement).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  userEvent.unhover(checkboxElement);
+  // this works as an assertion on its own so we don't need the "expect" assertion
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
